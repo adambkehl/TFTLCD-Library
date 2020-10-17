ca625a9328d57ab7ea000f1fd53352b3628e3d38
adambkehl <adambriankehl@gmail.com>
2020-10-17 14:23:58 -0700

    Optimize for ILI9341

--- Adafruit_TFTLCD.h
+++ Adafruit_TFTLCD.h
@@ -25,7 +25,7 @@ public:
   Adafruit_TFTLCD(uint8_t cs, uint8_t cd, uint8_t wr, uint8_t rd, uint8_t rst);
   Adafruit_TFTLCD(void);
 
-  void begin(uint16_t id = 0x9325);
+  void begin();
   void drawPixel(int16_t x, int16_t y, uint16_t color);
   void drawFastHLine(int16_t x0, int16_t y0, int16_t w, uint16_t color);
   void drawFastVLine(int16_t x0, int16_t y0, int16_t h, uint16_t color);
@@ -39,8 +39,6 @@ public:
   void setAddrWindow(int x1, int y1, int x2, int y2);
   void pushColors(uint16_t *data, uint8_t len, boolean first);
 
-  uint16_t color565(uint8_t r, uint8_t g, uint8_t b),
-      readPixel(int16_t x, int16_t y), readID(void);
   uint32_t readReg(uint8_t r);
 
 private:
